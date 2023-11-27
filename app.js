b87448242673f3254b935bda22b5b55a97e64ad0
vmlayton87 <vmlayton87@gmail.com>
2023-11-27 16:12:16 -0500

    solution with first bonus

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 class Casino {
     // Write code here
+    constructor(name, fakeCoin) {
+        this.name = name
+        this.timesPlayed = 0
+        this.isFakeCoin = fakeCoin
+    }
+    playGame(number) {
+        betAmount: number
+
+        if (Math.random() <= 0.5 || this.isFakeCoin === true) {console.log(this.name + "wins!")} 
+        else {console.log("You win $" + number + "!")}
+        this.timesPlayed = this.timesPlayed +1
+        console.log("You have now played " + this.timesPlayed + " times.")
+    }
 };
 
 // TESTS
+/*
 const myCasino = new Casino("HackerU Casino");
 console.log(myCasino);
 myCasino.playGame(5);
 myCasino.playGame(15);
 myCasino.playGame(25);
 myCasino.playGame(35);
-
+*/
 // BONUS TESTS
 /*
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
